577e4ffdf09a988d8ad5a34d630238c8a421f005
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-03-13 21:52:43 +0530

    v1.2
    Final Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-#MI_CARD
-<h3>This is a small app for contact information in a single page!</h3>
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,34 +14,99 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                color: Colors.red,
-                height: double.infinity,
-                width: 100.0,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/crop.jpg"),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                ],
+              Text(
+                'Abhishek Doshi',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                color: Colors.blue,
-                height: double.infinity,
-                width: 100.0,
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    '+91 7818044311',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                  onTap: () {
+                    launch('tel:+917818044311');
+                  },
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    'adoshi26.ad@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                  onTap: () {
+                    launch('mailto:adoshi26.ad@gmail.com');
+                  },
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.bookmark,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    'Twitter: @AbhishekDoshi26',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://twitter.com/AbhishekDoshi26');
+                  },
+                ),
               ),
             ],
           ),
